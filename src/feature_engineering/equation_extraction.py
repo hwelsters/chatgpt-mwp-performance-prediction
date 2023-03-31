e0f93b470edb4aea9eb08a887c7d8bad6f8aac99
hwelsters <84760072+hwelsters@users.noreply.github.com>
2023-03-31 02:47:14 -0700

    Equation extraction

--- src/feature_engineering/equation_extraction.py
+++ src/feature_engineering/equation_extraction.py
@@ -4,5 +4,25 @@ import re
 class EquationExtraction:
     @staticmethod
     def extract_equations(text : str):
-        
-        pass
+        text = text.replace('  ', '\n')
+        text = re.sub(r'\([a-zA-Z][a-zA-Z]', '\n', text)
+
+        split_text = text.split('\n')
+
+        equations = []
+        for t in split_text:
+            temp = re.sub('\+|\-|\/|\*|[0-9]|\s|=|\(|\)|\÷', ' ', t)
+            
+            failed = False
+
+            token = nltk.word_tokenize(temp)
+            pos_tagged = nltk.pos_tag(token)
+
+            for tag in pos_tagged:
+                if tag[1][0:2] == 'VB': failed = True
+
+            if t.count('=') + t.count('>') + t.count('<') + t.count('≤') + t.count('≥') == 0: failed = True
+            
+            if not failed: equations.append(t.strip())
+            
+        return equations

68bd13da091ae5d2adf83703eb38063bae289a17
hwelsters <84760072+hwelsters@users.noreply.github.com>
2023-04-06 10:11:44 -0700

    Split into multiple versions

---
+++ v1/src/feature_engineering/equation_extraction.py
@@ -0,0 +1,37 @@
+import nltk
+import re
+
+from bs4 import BeautifulSoup
+
+class EquationExtraction:
+    @staticmethod
+    def extract_equations(text : str):
+        text = text.replace('  ', '\n')
+        text = re.sub(r'\([a-zA-Z][a-zA-Z]', '\n', text)
+
+        split_text = text.split('\n')
+
+        equations = []
+        for t in split_text:
+            temp = re.sub('\+|\-|\/|\*|[0-9]|\s|=|\(|\)|\÷', ' ', t)
+            
+            failed = False
+
+            token = nltk.word_tokenize(temp)
+            pos_tagged = nltk.pos_tag(token)
+
+            for tag in pos_tagged:
+                if tag[1][0:2] == 'VB': failed = True
+
+            if t.count('=') + t.count('>') + t.count('<') + t.count('≤') + t.count('≥') == 0: failed = True
+            
+            if not failed: equations.append(t.strip())
+            
+        return equations
+
+    # @staticmethod
+    # def extract_equations(text: str):
+    #     soup = BeautifulSoup(text, "html.parser")
+    #     equations = soup.findAll("equation")
+    #     equations = [equation.text for equation in equations]
+    #     return equations
